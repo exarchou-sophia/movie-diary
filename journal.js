4c19e3108091ecba6be1742960b461308c597287
sogol mofrad <sogolmofrad@gmail.com>
2024-09-11 18:47:09 +0200

    removing type from script tag

--- journal.js
+++ journal.js
@@ -1,7 +1,8 @@
-const favMovie = JSON.parse(localStorage.getItem('favList'))
+const favMovie = JSON.parse(localStorage.getItem("favList"));
 document.addEventListener("DOMContentLoaded", async () => {
     try {
-        const popMoviesFav = document.getElementById("popularMovieList")
+        const popMoviesFav = document.getElementById("popularMovieList");
+        console.log(favMovie);
         favMovie.forEach(movie => {
             const item = document.createElement("li");
             item.innerHTML = `
@@ -12,15 +13,41 @@ document.addEventListener("DOMContentLoaded", async () => {
             <span>${movie.releaseDate}</span>
             </div>
             <button class="w-[80%] mt-[1rem] py-[5px] px-[2rem] text-[1.4rem] bg-[#020F1D] rounded-full">+ Delete from Favorites</button>
-            `
+            `;
             item.classList.add("flex", "flex-col", "justify-between");
             popMoviesFav.appendChild(item);
-        })
+        });
     } catch (error) {
         console.error(error);
     }
 });
 
+document.addEventListener("DOMContentLoaded", async () => {
+    try {
+        const searchInputView = document.getElementById("searchInput");
+        searchInputView.addEventListener("change", async event => {
+            const userSearchText = event.target.value;
+            console.log("user is searching for", userSearchText);
+
+            const titleOfMovie = await getMoviesByTitle(userSearchText);
+            const searchBarResult = document.getElementById("searchResults");
+            // reset search results
+            searchBarResult.innerHTML = "";
+
+            console.log(titleOfMovie);
+            titleOfMovie.forEach(movie => {
+                console.log("movie.poster_path}", movie.poster_path);
+
+                const item = document.createElement("li");
+                item.textContent = movie.original_title;
+                item.style = `background-position: center; background-repeat: no-repeat; background-size: contain; color: white; background-image: url('${movie.poster_path}'); width: 100px; height: 100px`;
+                searchBarResult.appendChild(item);
+            });
+        });
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 // const movie2 = JSON.parse(localStorage.getItem('movie'))
 // console.log(movie2)
