99746f05aa06ae627ff1d16d37612a699aa8c061
Manuel <manuel.penner@mein.gmx>
2024-09-11 12:07:53 +0200

    changed data population in card

--- journal.js
+++ journal.js
@@ -1,42 +1,51 @@
-
-const movie2 = JSON.parse(localStorage.getItem('movie'))
-console.log(movie2)
+const favMovie = JSON.parse(localStorage.getItem('favList'))
 document.addEventListener("DOMContentLoaded", async () => {
     try {
-        const popularMovies = await movie2;
-        const popMoviesView = document.getElementById("favMovieList");
-        popularMovies.forEach(movie => {
-            console.log("movie.poster_path", movie.poster_path);
-            console.log(movie);
-
+        const popMoviesView = document.getElementById("popularMovieList")
+        favMovie.forEach(movie => {
             const item = document.createElement("li");
-
             item.innerHTML = `
-            <a href="#"><img src="${movie.poster_path}" alt="movie poste" class="w-full rounded-[3rem] shadow-lg"/></a>
+            <a href="#"><img src="${movie.imgSrc}" alt="movie poste" class="w-full rounded-[3rem] shadow-lg"/></a>
             <div class="mt-[7px] pl-[2rem]">
             <h4 class="text-[1.4rem] md:text-[1.6rem]">${movie.title}</h4>
-
-            <span>⭐️ ${movie.vote_average.toFixed(1)}</span>
-            <span>| ${movie.release_date.split("-")[0]}</span>
+            <span>${movie.votes}</span>
+            <span>${movie.releaseDate}</span>
             </div>
-            `;
+            `
             item.classList.add("flex", "flex-col", "justify-between");
             popMoviesView.appendChild(item);
-        });
+        })
     } catch (error) {
         console.error(error);
     }
 });
 
-function card() {
-    return `
-            <a href="#"><img src=${
-                movie.poster_path
-            } alt="movie poste" class="w-full rounded-[3rem] shadow-lg"/></a>
-            <div class="mt-[7px] pl-[2rem]">
-            <h4 class="text-[1.4rem] md:text-[1.6rem]">${movie.title}</h4>
 
-            <span>⭐️ ${movie.vote_average.toFixed(1)}</span>
-            <span>| ${movie.release_date.split("-")[0]}</span>
-            </div>`
-}
+// const movie2 = JSON.parse(localStorage.getItem('movie'))
+// console.log(movie2)
+// document.addEventListener("DOMContentLoaded", async () => {
+// try {
+//     const popularMovies = await movie2;
+//     const popMoviesView = document.getElementById("popularMovieList");
+//     popularMovies.forEach(movie => {
+//         console.log("movie.poster_path", movie.poster_path);
+//         console.log(movie);
+
+//         const item = document.createElement("li");
+
+//         item.innerHTML = `
+//         <a href="#"><img src="${movie.poster_path}" alt="movie poste" class="w-full rounded-[3rem] shadow-lg"/></a>
+//         <div class="mt-[7px] pl-[2rem]">
+//         <h4 class="text-[1.4rem] md:text-[1.6rem]">${movie.title}</h4>
+
+//         <span>⭐️ ${movie.vote_average.toFixed(1)}</span>
+//         <span>| ${movie.release_date.split("-")[0]}</span>
+//         </div>
+//         `;
+//         item.classList.add("flex", "flex-col", "justify-between");
+//         popMoviesView.appendChild(item);
+//     });
+// } catch (error) {
+//     console.error(error);
+// }
+// });
